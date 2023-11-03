3118f8b6bc757b162685fbb54347a2429d2c7d5a
Flavie Bourcet <flavie.bourcet@gmail.com>
2023-11-03 17:12:58 +0100

    feat recipes cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,9 @@
+import Card from "./template/card/index.js";
+
+const body = document.querySelector("body");
+
 function createHeader() {
-  const body = document.querySelector("body");
-
   const header = document.createElement("header");
-
-  body.appendChild(header);
-
   const logo = document.createElement("img");
   logo.classList.add("logo");
   logo.setAttribute("src", "./assets/logo.svg");
@@ -14,15 +13,19 @@ function createHeader() {
 
   const title = document.createElement("h1");
   title.classList.add("title");
-  title.innerHTML = "CHERCHEZ PARMI PLUS DE 1500 RECETTES DU QUOTIDIEN,SIMPLES ET DÉLICIEUSES";
+  title.innerHTML =
+    "CHERCHEZ PARMI PLUS DE 1500 RECETTES DU QUOTIDIEN,SIMPLES ET DÉLICIEUSES";
 
   header.appendChild(title);
 
   const searchBarContainer = document.createElement("div");
-  searchBarContainer.classList.add("searchbarContainer");
+  searchBarContainer.classList.add("searchbar-container");
 
   const searchBar = document.createElement("input");
-  searchBar.setAttribute("placeholder", "Rechercher une recette, un ingrédient, ...")
+  searchBar.setAttribute(
+    "placeholder",
+    "Rechercher une recette, un ingrédient, ..."
+  );
   searchBar.classList.add("searchbar");
 
   const searchButton = document.createElement("button");
@@ -37,6 +40,30 @@ function createHeader() {
   searchBarContainer.appendChild(searchButton);
 
   header.appendChild(searchBarContainer);
+
+  return header;
 }
 
-createHeader();
+function cardsContainer() {
+  const container = document.createElement("div");
+  container.classList.add("cards-container");
+
+  fetch("http://127.0.0.1:5500/api/recipes.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((element) => {
+        console.log(element);
+        container.appendChild(
+          Card(element.image,element.name, element.description, element.ingredients, element.time)
+        );
+      });
+    });
+
+  return container;
+}
+body.appendChild(createHeader());
+
+const main = document.createElement("main");
+body.appendChild(main);
+
+main.appendChild(cardsContainer());
---
+++ scripts/template/card/index.js
@@ -0,0 +1,78 @@
+function Card(imageLink, title, recipe, ingredients, timer) {
+  const card = document.createElement("article");
+  card.classList.add("card");
+
+  const cardHeader = document.createElement("img");
+  cardHeader.setAttribute("src", `assets/photos/${imageLink}`);
+  card.appendChild(cardHeader);
+
+  const cardTitle = document.createElement("h2");
+  cardTitle.classList.add("recipe-title");
+  cardTitle.innerHTML = title;
+  card.appendChild(cardTitle);
+
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  card.appendChild(cardBody);
+
+  const recipeContainer = document.createElement("div");
+  recipeContainer.classList.add("recipe-container");
+
+  cardBody.appendChild(recipeContainer);
+
+  const recipeTitle = document.createElement("h3");
+  recipeTitle.classList.add("title");
+  recipeTitle.innerHTML = "Recette";
+
+  recipeContainer.appendChild(recipeTitle);
+
+  const recipeContent = document.createElement("p");
+  recipeContent.innerHTML = recipe;
+
+  recipeContainer.appendChild(recipeContent);
+
+  const ingredientsContainer = document.createElement("div");
+  ingredientsContainer.classList.add("ingredients-container");
+
+  cardBody.appendChild(ingredientsContainer);
+
+  const ingredientsTitle = document.createElement("h3");
+  ingredientsTitle.classList.add("title");
+  ingredientsTitle.innerHTML = "Ingrédients";
+
+  ingredientsContainer.appendChild(ingredientsTitle);
+
+  const ingredientsList = document.createElement("ul");
+  ingredients.forEach((element) => {
+    const ingredientItem = document.createElement("li");
+    ingredientItem.setAttribute("key", element.ingredient);
+
+    const ingredientName = document.createElement("h4");
+    ingredientName.innerHTML = `${element.ingredient}`;
+    ingredientItem.appendChild(ingredientName);
+
+    const ingredientQuantity = document.createElement("p");
+    element.quantity ? (ingredientQuantity.innerHTML = element.quantity) : null;
+    element.unit
+      ? ingredientQuantity.insertAdjacentHTML("beforeend", element.unit)
+      : null;
+    // ingredientQuantity.innerHTML = `${element.quantity ? element.quantity : ""} ${element.unit ? element.unit : ""}`;
+    ingredientItem.appendChild(ingredientQuantity);
+
+    ingredientsList.appendChild(ingredientItem);
+  });
+
+  const toast = document.createElement("div");
+  toast.classList.add("toast")
+  const time = document.createElement("p");
+  time.innerHTML = `${timer}min`;
+
+  toast.appendChild(time);
+  card.appendChild(toast);
+
+  ingredientsContainer.appendChild(ingredientsList);
+
+  return card;
+}
+
+export default Card;
